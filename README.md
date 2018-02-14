6fdbdfcc9a1b66fcee80b725dd5d80ea8db83a2d
Aditya Jain <adityajn105@gmail.com>
2018-02-15 01:08:00 +0530

    flask app completed

--- README.md
+++ README.md
@@ -1,16 +1,43 @@
 # Spam-Ham-Email-Classifier
-This is a Flask Web App for classifying emails into Spam and Ham. The algorithm uses various Algorithm.
+This is a Flask Web App for classifying emails into Spam and Ham. The algorithm uses various libraries.
 
 The Pipeline is given Below
 
 1. Remove Stop words via Sckit CountVectorizer
-2. Bag Of Words Algorithm  via Sckit CountVectorizer
-3. TF-IDF
-4. Multinomial Naive Bayes Classifier
+2. Stemming Words via NLTK
+3. Bag Of Words Algorithm  via Sckit CountVectorizer
+4. TF-IDF
+5. Multinomial Naive Bayes Classifier
 
 More things to be implemented
-GridCV
-Stemming - NLTK to permform stemming consider words like fish,fishing,fished as same.
+1. GridCV
 
 
+## Installing NLTK via Command Line
+Run the command 
+```
+python -m nltk.downloader all
+``` 
+To ensure central installation, run the command 
+```
+sudo python -m nltk.downloader -d /usr/local/share/nltk_data all.
+```
 
+ You can use the -d flag to specify a different location (but if you do this, be sure to set the NLTK_DATA environment variable accordingly).
+
+
+## How to run
+Required Python Libraries
+1. python-flask
+2. pandas
+3. numpy
+3. sckit-learn
+4. nltk
+
+To run app:
+```
+	python app.py
+```
+Initializing classifier may take about half and hour due to large training dataset So wait patiently .
+
+The app by default will run on port 3000. You can change it from app.js.
--- app.py
+++ app.py
@@ -17,18 +17,72 @@ from sklearn.naive_bayes import MultinomialNB
 
 app = Flask(__name__)
 
+count_vect = None # count vectorizer 
+tfidf_transformer = None	# tfidf transformer
+clf = None	# multinomial classifier
+
+
 class StemmedCountVectorizer(CountVectorizer):
     def build_analyzer(self):
         stemmer = SnowballStemmer("english", ignore_stopwords=True)
         analyzer = super(StemmedCountVectorizer, self).build_analyzer()
         return lambda doc: ([stemmer.stem(w) for w in analyzer(doc)])
 
-@app.route('/')
-def home(name=None):
-	return render_template('Booths-Multiplier.html',data = {'status':False}, name=name) 
 
-@app.route('/', methods = ['POST'])
-def predict():
-	
+@app.route('/', methods = ['POST','GET'])
+def predict(name=None):
+	if request.method == 'POST':
+		data = request.form
+		text = data['content']
+		X_predict = count_vect.transform(np.array([text]))
+		X_predict_tfidf = tfidf_transformer.transform(X_predict)
+		prediction = clf.predict(X_predict_tfidf)
+		return render_template('index.html',data = { 'status' : True, 'result' : prediction[0] ,'content' : '"'+text+'"' } ) 
+	else:
+		return render_template('index.html',data = { 'status':False }, name=name) 
+
+
 if __name__ == '__main__':
+    print("Initializing Classifier... Please Wait...")
+    #getting emails dataframe
+    emails = pd.read_csv('DataSets/index/train.csv',sep=" ",header=None,names=['category','paths'])
+    emails['body'] = ''
+    for index in range(0,len(emails.paths)):
+    	fp = codecs.open("DataSets"+emails.paths[index][2:], "r",encoding='utf-8', errors='ignore')
+    	email_text = email.message_from_file(fp)
+    	texts = ""
+    	if  email_text.is_multipart():
+    		for part in email_text.get_payload():
+    			if part.get_content_maintype() == 'text':
+    				texts += part.get_payload()
+    	else:
+    		texts += email_text.get_payload()
+    	texts = re.sub('\n','',texts)
+    	texts = re.sub('_','',texts)
+    	texts = re.sub('-',' ',texts)
+    	texts = re.sub('/',' ',texts)
+    	texts = re.sub(':',' ',texts)
+    	texts = re.sub('$','',texts)
+    	texts = re.sub('=','',texts)
+    	texts = re.sub('<.*?>', '', texts)
+    	emails.body[index] = " ".join((BeautifulSoup(texts.lower(), 'html.parser').findAll(text=True)))
+    	fp.close()
+    #removing samples with empty training body    
+    temails = emails[emails.body!='']
+    temails=temails[['category','body']]
+
+    #making spam and ham samples equal
+    te_spam=temails.loc[temails['category']=='spam'].sample(30000,replace=True)
+    te_ham=temails.loc[temails['category']=='ham'].sample(30000,replace=True)
+    temails=pd.concat([te_spam,te_ham])
+    temails.groupby('category').size()
+
+    count_vect = StemmedCountVectorizer(stop_words='english').fit(temails.body)
+    X_train_counts = count_vect.transform(temails.body)
+    
+    tfidf_transformer = TfidfTransformer().fit(X_train_counts)
+    X_train_tfidf = tfidf_transformer.transform(X_train_counts)
+
+    clf = MultinomialNB().fit(X_train_tfidf, temails.category)
+    print("Classifier Initialized with Accuracy {}".format(clf.score(X_train_tfidf, temails.category)))
     app.run(host = '0.0.0.0', port = 3000)
